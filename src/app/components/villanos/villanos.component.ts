922e0263def1a588a17cb720df5c3d9972e61378
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2020-05-24 11:16:23 -0500

    first commit

---
+++ src/app/components/villanos/villanos.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-villanos',
+  templateUrl: './villanos.component.html'
+})
+export class VillanosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
