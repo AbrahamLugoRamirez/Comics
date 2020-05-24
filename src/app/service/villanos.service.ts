cf905a385b80f255f0a7ee5e5a98605760585289
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2020-05-24 14:59:56 -0500

    Images villanos

--- src/app/service/villanos.service.ts
+++ src/app/service/villanos.service.ts
@@ -8,51 +8,51 @@ export class VillanosService {
             {
               nombre: "Thanos",
               bio: "Thanos es un Titán que nació en el planeta con el mismo nombre. Originalmente fue creado por Jim Starlin y Mike Friedrich como uno de los enemigos de Iron-Man en 1973.",
-              img: "assets/img/aquaman.png",
+              img: "assets/villanos/thanos.png",
               aparicion: "1973-02-01",
               casa:"Marvel"
             },
             {
               nombre: "Ultron",
               bio: "es un ficticio supervillano que aparece en los cómics americanos publicados por Marvel . Es más reconocido como un enemigo del grupo de superhéroes de los Vengadores y su relación cuasifamiliar con su creador Hank Pym .",
-              img: "assets/img/batman.png",
+              img: "assets/villanos/Ultron.jpg",
               aparicion: "1968-06-01",
               casa:"Marvel"
             },
             {
-              nombre: "Daredevil",
-              bio: "Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aumentados por la radiación a niveles superhumanos, en el accidente que tuvo cuando era niño. A pesar de su ceguera, puede \"ver\" a través de un \"sexto sentido\" que le sirve como un radar similar al de los murciélagos.",
-              img: "assets/img/daredevil.png",
-              aparicion: "1964-01-01",
+              nombre: "Magneto",
+              bio: "Mutante capaz de controlar los campos magnéticos, pudiendo manipular el metal con su mente. Suele entrar en conflicto con los héroes de Marvel debido su ideología supremacista, que propugna suplantar a la especie humana en favor de la población mutante.",
+              img: "assets/villanos/magneto.jpg",
+              aparicion: "1963-08-01",
               casa: "Marvel"
             },
             {
-              nombre: "Hulk",
-              bio: "Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticamente ilimitados a la vez que aumenta su furia. Dependiendo de qué personalidad de Hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo compensa con su inteligencia).",
-              img: "assets/img/hulk.png",
-              aparicion: "1962-05-01",
+              nombre: "Apocalipsis",
+              bio: "Gran enemigo de los X-men, yendo un paso más allá de los postulados del Amo del Magnetismo. En Sabah Nur (nombre real de este maloso) es el primer mutante de la historia, nacido allá por el Antiguo Egipto, Fue criado por una tribu nómada que le inculcó el principio de la supervivencia del más fuerte, por el cual basaría toda su existencia.",
+              img: "assets/villanos/apocalipsis.jpg",
+              aparicion: "1986-06-01",
               casa:"Marvel"
             },
             {
-              nombre: "Linterna Verde",
-              bio: "Poseedor del anillo de poder que posee la capacidad de crear manifestaciones de luz sólida mediante la utilización del pensamiento. Es alimentado por la Llama Verde (revisada por escritores posteriores como un poder místico llamado Starheart), una llama mágica contenida en dentro de un orbe (el orbe era en realidad un meteorito verde de metal que cayó a la Tierra, el cual encontró un fabricante de lámparas llamado Chang)",
-              img: "assets/img/linterna-verde.png",
-              aparicion: "1940-06-01",
+              nombre: "Joker",
+              bio: "The Joker es un supervillano creado por Bill Finger , Bob Kane y Jerry Robinson, que apareció por primera vez en el primer número del cómic Batman (25 de abril de 1940), publicado por DC Comics . El crédito por la creación del Joker se disputa; Kane y Robinson se atribuyeron la responsabilidad del diseño del Joker al tiempo que reconocieron la contribución escrita de Finger.",
+              img: "assets/villanos/joker.jpg",
+              aparicion: "1940-04-01",
               casa: "DC"
             },
             {
-              nombre: "Spider-Man",
-              bio: "Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehumanos, una gran fuerza, agilidad, poder trepar por paredes. La fuerza de Spider-Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Man puede realizar saltos íncreibles. Un \"sentido arácnido\", que le permite saber si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede llevar a Spider-Man al origen del peligro.",
-              img: "assets/img/spiderman.png",
-              aparicion: "1962-08-01",
-              casa: "Marvel"
+              nombre: "Lex Luthor",
+              bio: "Originalmente presentado como un científico loco cuyos esquemas Superman frustraría rutinariamente, la representación de Lex ha evolucionado a lo largo de los años y su caracterización se ha profundizado. En las historias contemporáneas, Lex es retratado como un magnate de los negocios estadounidense rico, loco por el poder , ingenioso ingeniero, filántropo de la ciudad de Metrópolis y una de las personas más inteligentes del mundo.",
+              img: "assets/villanos/lex.png",
+              aparicion: "1940-04-01",
+              casa: "DC"
             },
             {
-              nombre: "Wolverine",
-              bio: "En el universo ficticio de Marvel, Wolverine posee poderes regenerativos que pueden curar cualquier herida, por mortal que ésta sea, además ese mismo poder hace que sea inmune a cualquier enfermedad existente en la Tierra y algunas extraterrestres . Posee también una fuerza sobrehumana, que si bien no se compara con la de otros superhéroes como Hulk, sí sobrepasa la de cualquier humano.",
-              img: "assets/img/wolverine.png",
-              aparicion: "1974-11-01",
-              casa: "Marvel"
+              nombre: "Darkseid",
+              bio: "Como el gobernante tiránico y antiguo príncipe del planeta Apokolips , Darkseid es un Dios del Mal superior interdimensional extremadamente poderoso y peligroso. El objetivo final de Darkseid es conquistar el universo y eliminar todo el libre albedrío y los seres sintientes.",
+              img: "assets/villanos/darkseid.jpg",
+              aparicion: "1970-11-01",
+              casa: "DC"
             }
     ];
 
